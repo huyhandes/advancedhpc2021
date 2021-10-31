8246516d9c4f638fc22e0207458659d251f788e3
Handes <43632522+huyhandes@users.noreply.github.com>
2021-10-31 13:15:38 +0700

    [Labwork] add miltiprocess for labwork 1

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -108,6 +108,16 @@ void Labwork::labwork1_CPU() {
 
 void Labwork::labwork1_OpenMP() {
     int pixelCount = inputImage->width * inputImage->height;
+    omp_set_num_threads(15);
+    #pragma omp parallel for
+    for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
+        for (int i = 0; i < pixelCount; i++) {
+            outputImage[i * 3] = (char) (((int) inputImage->buffer[i * 3] + (int) inputImage->buffer[i * 3 + 1] +
+                                          (int) inputImage->buffer[i * 3 + 2]) / 3);
+            outputImage[i * 3 + 1] = outputImage[i * 3];
+            outputImage[i * 3 + 2] = outputImage[i * 3];
+        }
+    }
     outputImage = static_cast<char *>(malloc(pixelCount * 3));
     // do something here
 }
